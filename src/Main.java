9b28d6b6beca127e45059abcf1ce114e12b8d495
ruanbauerr <ruanbauer4@gmail.com>
2024-08-22 06:09:38 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Ruan");
+
+        //TipoDaVariavel nomeVariavel = valorDaVariavel
+        //String é um TEXTO
+        String nome = "Ruan";
+        char sexo = 'M';
+        // [] - Diz que é um array
+        // {} - Indicar um objeto
+        char[] textoChar = {'5'};
+
+        //Guardar Valores Reais
+        double altura = 1.53;
+        Double peso = 15.0;
+        float alturaPessoa2 = 1.80f;
+        Float alturaPessoa3 = 1.75f;
+        int NumeroCasa= 20;
+        Integer numeroAlunos = 1;
+        long numerogols = 31;
+
+        //Operadores aritmeticos
+        //+ para soma
+        //       - para subtração
+        //para multiplicação
+        //para divisão
+
+        Integer soma = 5 + 5;
+        Integer subtração = 5 - 2;
+        Integer multiplicar = 5 * 5;
+        Integer dividir = 5 / 5;
+
+        Integer resultadoSoma = soma + subtração;
+        Integer resultadoSubtração = soma - subtração;
+        Integer resultadoMultiplicar = soma * subtração;
+        Integer resultadoDivisao = soma / subtração;
+
+        System.out.println("Valor da divisão é : " + resultadoDivisao);
+        System.out.println("Valor da multiplicação é : " + resultadoMultiplicar);
+        System.out.println("Valor da divisão é : " + resultadoSoma);
+        System.out.println("Valor da divisão é : " + resultadoSubtração);
+
+
+
+
+
+
+
+
+
+
     }
 }
